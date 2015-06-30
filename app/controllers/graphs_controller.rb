d7f1194343ca81099be6f8f75949b35a26ba088f
SpookyJones <spookyjonesmusic@gmail.com>
2015-06-30 14:22:37 -0400

    added graph for avg price over last year

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,22 +1,45 @@
 class GraphsController < ApplicationController
-protect_from_forgery
-
+	
+	protect_from_forgery
+	
   def index
-    @chart = LazyHighCharts::HighChart.new('graph') do |f|
+  	@region_names = []
+	  @region_lowest = []
+	  @region_average =[]
+	  @region_highest = []
+  	@regions = Region.all
+	  @regions.each do |r| 
+	  @region_names << r.name #adds each region name to an array to pass for next arg
+		@region_lowest << r.prices.minimum(:cost).to_i
+		@region_highest << r.prices.maximum(:cost).to_i
+		@region_average << r.prices.average(:cost).to_i
+	  end	
+	
+	
+	@chart = LazyHighCharts::HighChart.new('graph') do |f|
+	
       f.title({ :text=>"Combination chart"})
-      f.options[:xAxis][:categories] = ['Apples', 'Oranges', 'Pears', 'Bananas', 'Plums']
-      f.labels(:items=>[:html=>"Total fruit consumption", :style=>{:left=>"40px", :top=>"8px", :color=>"black"} ])      
-      f.series(:type=> 'column',:name=> 'Jane',:data=> [3, 2, 1, 3, 4])
-      f.series(:type=> 'column',:name=> 'John',:data=> [2, 3, 5, 7, 6])
-      f.series(:type=> 'column', :name=> 'Joe',:data=> [4, 3, 3, 9, 0])
-      f.series(:type=> 'spline',:name=> 'Average', :data=> [3, 2.67, 3, 6.33, 3.33])
+      f.options[:xAxis][:categories] = @region_names
+	    f.labels(:items=>[:html=>"Total Data Points", :style=>{:left=>"40px", :top=>"8px", :color=>"black"} ])
+      f.series(:type=> 'column',:name=> 'Lowest',:data=> @region_lowest)
+      f.series(:type=> 'column',:name=> 'Average',:data=> @region_average)
+      f.series(:type=> 'column', :name=> 'Highest',:data=> @region_highest)
+      f.series(:type=> 'spline',:name=> 'Average', :data=> @region_average)
       f.series(:type=> 'pie',:name=> 'Total consumption', 
         :data=> [
-          {:name=> 'Jane', :y=> 13, :color=> 'red'}, 
-          {:name=> 'John', :y=> 23,:color=> 'green'},
-          {:name=> 'Joe', :y=> 19,:color=> 'blue'}
+          {:name=> 'Indica', :y=> 13, :color=> 'red'}, 
+          {:name=> 'Sative', :y=> 23,:color=> 'green'},
+          {:name=> 'Hybrid', :y=> 19,:color=> 'blue'}
         ],
         :center=> [100, 80], :size=> 100, :showInLegend=> false)
+    end
+	
+    @states_chart = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title({ :text=>"Combination chart"})
+	   	f.options[:xAxis][:categories] = @region_names
+      f.series(:type=> 'column',:name=> 'Lowest',:data=> @region_lowest)
+      f.series(:type=> 'column',:name=> 'Average',:data=> @region_average)
+      f.series(:type=> 'column', :name=> 'Highest',:data=> @region_highest)
     end
 
     @chart2 = LazyHighCharts::HighChart.new('graph') do |f|
@@ -24,12 +47,10 @@ protect_from_forgery
       f.xAxis(:categories => ["United States", "Japan", "China", "Germany", "France"])
       f.series(:name => "GDP in Billions", :yAxis => 0, :data => [14119, 5068, 4985, 3339, 2656])
       f.series(:name => "Population in Millions", :yAxis => 1, :data => [310, 127, 1340, 81, 65])
-
       f.yAxis [
         {:title => {:text => "GDP in Billions", :margin => 70} },
         {:title => {:text => "Population in Millions"}, :opposite => true},
       ]
-
       f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical',)
       f.chart({:defaultSeriesType=>"column"})
     end
@@ -76,17 +97,62 @@ protect_from_forgery
       f.options[:chart][:defaultSeriesType] = "column"
       f.plot_options({:column=>{:stacking=>"percent"}})
     end
-	
-	@strains = Strain.all
+
 		
+		
+	start_date = 12.months.ago.to_date
+# => Wed, 16 Nov 2011
+
+	end_date = Date.today
+# => Sun, 16 Dec 2012
+
+	number_of_months = (end_date.year*12+end_date.month)-(start_date.year*12+start_date.month)
+# => 13
+	@last_year_in_months = number_of_months.times.each_with_object([]) do |count, array|
+	  array << [start_date.beginning_of_month + count.months,
+	            start_date.end_of_month + count.months]
+	end
+	
+
+  @months = []
+	@months_avg = []
+  @avg_month_prices = []
+  @avg_day_prices = []
+  
+	@last_year_in_months.each do |m| 
+		@months << Date::MONTHNAMES[m.first.month] + " " + m.first.year.to_s 
+  end
+  
+
+   
+   
+  13.times do |m|
+    n = m + 1
+    avg = avg_price_month(n.months.ago, (n - 1).months.ago)
+    avg == Float::INFINITY ? avg = 0 : avg = avg
+    @avg_month_prices << avg 
+  end
+  
+  
+  @date = Date.today
+  @date.strftime("%B")
+  @months <<  Date::MONTHNAMES[@date.month] + " " + @date.year.to_s
+
+  
+  365.times do |m|
+    n = m + 1
+    avg = avg = avg_price_month(n.days.ago, (n - 1).days.ago)
+    avg == Float::INFINITY ? avg = 0 : avg = avg
+    @avg_day_prices << avg 
+  end
+  
 	 @prices_chart = LazyHighCharts::HighChart.new('graph') do |f|
 	 f.title(:text => "Avg Price of Marijuana over time")
-	 f.xAxis(:categories => ["United States", "Japan", "China", "Germany", "France"])
-	 f.series(:name => "GDP in Billions", :yAxis => 0, :data => [14119, 5068, 4985, 3339, 2656])
-	 f.series(:name => "Population in Millions", :yAxis => 1, :data => [310, 127, 1340, 81, 65])
+	 f.xAxis(:categories => @months)
+	 f.series(:name => "Average Price", :yAxis => 0, :data => @avg_month_prices.reverse)
 			
 	 f.yAxis [
-	   {:title => {:text => "GDP in Billions", :margin => 70} },
+	   {:title => {:text => "Average Price", :margin => 70} },
 	   {:title => {:text => "Population in Millions"}, :opposite => true},
 	 ]
 			
@@ -96,5 +162,33 @@ protect_from_forgery
 	
 	
 	end
+
+private
+
+	def last_year_months
+		start_date = 12.months.ago.to_date
+	# => Wed, 16 Nov 2011
+	
+		end_date = Date.today
+	# => Sun, 16 Dec 2012
+	
+		number_of_months = (end_date.year*12+end_date.month)-(start_date.year*12+start_date.month)
+	# => 13
+		@last_year_in_months = number_of_months.times.each_with_object([]) do |count, array|
+		  array << [start_date.beginning_of_month + count.months,
+		            start_date.end_of_month + count.months]
+		end
+	end
+	
+	def avg_price_month(start, stop)
+		total = 0.00
+		@last_price = Price.where("created_at > ? AND created_at < ?", start, stop ) 
+		@last_price.each do |n| 
+			@total = @total.to_f + n.cost.to_f 
+		end 
+		(@total.to_f / @last_price.count.to_f)
+	end
+	
+
 
 end
--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -1,2 +1,12 @@
 module GraphsHelper
+	
+	def avg_price_month(start, stop)
+		total = 0.00
+		@last_price = Price.where("created_at > ? AND created_at < ?", start, stop ) 
+		@last_price.each do |n| 
+			@total = @total.to_f + n.cost.to_f 
+		end 
+		(@total.to_f / @last_price.count.to_f)
+	end
+	
 end
