1bf6060742a6a10918f5a1011e60e79f02dcd411
SpookyJones <spookyjonesmusic@gmail.com>
2015-08-01 11:27:34 -0400

    fixed git ignore

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    provider :facebook, ENV['APP_ID'], ENV['APP_SECRET'],
-    :scope => 'email,user_friends'
+    provider :facebook, ENV['APP_ID'], ENV['APP_SECRET'], :scope => 'email,user_friends,public_profile'
   else
-    provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'],
-    :scope => 'email,user_friends'
+    provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], :scope => 'email,user_friends,public_profile'
   end
 end
 
