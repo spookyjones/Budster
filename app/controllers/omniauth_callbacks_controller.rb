aa51b1b3872c702d1fa512c69b2f7cc848354e03
spookyjones <spookyjonesmusic@gmail.com>
2015-08-02 03:16:27 -0400

    added generated email from uid when facebook doesnt return a user email.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,14 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
 
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    
     if @user.persisted?
+      if @user.email = ""
+        @user.email = request.env["omniauth.auth"]["uid"]+"@facebook.com"
+      end
+      @user.email = @user.email 
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :omniauthable, :omniauth_providers => [:facebook]
-  
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      
-      auth[:info][:email] ? user.email = auth[:info][:email] : user.email = ""
+      auth[:info][:email] ? user.email = auth[:info][:email] : user.email = auth[:uid] + "@facebook.com"
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       user.image =  auth[:info][:image] # assuming the user model has an image
       user.last_sign_in_at = Time.now
-      user.info = auth.info
-      user.save
+      user.info = auth.inspect
     end
   end
 
@@ -28,9 +26,13 @@ class User < ActiveRecord::Base
   def largeimage
     "http://graph.facebook.com/#{self.uid}/picture?type=large"
   end
-  
+
   def normalimage
      "http://graph.facebook.com/#{self.uid}/picture?type=normal"
   end
-  
+
+  def smallimage
+     "http://graph.facebook.com/#{self.uid}/picture?type=small"
+  end
+
 end
